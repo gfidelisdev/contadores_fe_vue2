fd20cf3867519d08b4f628db5bccf40c44118a43
Guilherme Fidelis da Silva <gfidelisdev@gmail.com>
2022-12-29 15:20:51 -0300

    creation of fileexporter component, creates csv file

---
+++ src/components/FileExporter.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <a color="success" download="contadores.csv" :href="link">
+            <v-btn color="success" fab>
+                <v-icon> mdi-microsoft-excel </v-icon>
+            </v-btn>
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        jsonObjToCsv: {
+            type: Array,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            textFile: "",
+            link: "",
+        }
+    },
+    methods: {
+        createDataURL(text) {
+            var data = new Blob([text], { type: "text/plain" })
+            // If we are replacing a previously generated file we need to
+            // manually revoke the object URL to avoid memory leaks.
+            if (this.textFile !== null) {
+                window.URL.revokeObjectURL(this.textFile)
+            }
+
+            this.link = window.URL.createObjectURL(data)
+
+            // return textFile;
+        },
+
+        parseToCsv(jsonArray) {
+            let keys = Object.keys(jsonArray[0])
+            let headers = keys.join(";")
+            console.warn(headers)
+            let result = ""
+            for (let line of jsonArray) {
+                let objResult = []
+                for (let key of keys) {
+                    console.error(typeof line[key])
+                    if (typeof line[key] !== "object") {
+                        objResult.push(line[key])
+                    } else {
+                        objResult.push("-")
+                    }
+                }
+                result = `${result}${objResult.join(";")}\r\n`
+            }
+
+            return `${headers}\r\n${result}`
+        },
+    },
+    created() {
+        this.createDataURL(this.parseToCsv(this.jsonObjToCsv))
+    },
+}
+</script>
+
+<style>
+a {
+    text-decoration: none;
+}
+</style>
+:
