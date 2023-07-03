ed09fade55a46870301751970ff2dc2ef1322c19
Guilherme Fidelis da Silva <109247627+gfidelisdev@users.noreply.github.com>
2023-07-03 11:55:49 -0300

    20230703.debug

--- src/components/FailuresList.vue
+++ src/components/FailuresList.vue
@@ -46,7 +46,7 @@
                 <!-- <v-chip class="ma-2" :color="report.printer.status ? 'green' : 'red'" text-color="white">
                     {{ report.printer.status ? 'Online' : 'Offline' }}
                 </v-chip> -->
-                <v-btn :color="report.printer.status ? 'green' : 'red'" fab x-small></v-btn>
+                <v-btn :color="report.printer.status ? 'green' : 'red'" fab x-small>{{ report.printer.status }}</v-btn>
             </v-col>
 
             <v-col>
